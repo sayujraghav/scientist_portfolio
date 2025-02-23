2f6c94cd751cbf229e950fe2de20a9df02728022
Anoop <anoop.ps@digitalmesh.com>
2025-02-23 21:28:14 +0530

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,8 +11,6 @@ const Navbar = () => {
           <div className='space-x-6'>
             <a href='#home' className='hover:text-gray-400'>Home</a>
             <a href='#about' className='hover:text-gray-400'>About Me</a>
-            <a href='#services' className='hover:text-gray-400'>Services</a>
-            <a href='#projects' className='hover:text-gray-400'>Projects</a>
             <a href='#contact' className='hover:text-gray-400'>Contact</a>
           </div>
           <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Connect Me</button>
