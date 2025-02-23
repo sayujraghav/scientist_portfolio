71589eadb72ff4fac1e86d7fbbf8e9ccd74042b9
Anoop <anoop.ps@digitalmesh.com>
2025-02-23 21:18:29 +0530

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Set basename dynamically based on environment
+const basename =
+  process.env.NODE_ENV === "production" ? "/scientist_portfolio" : "/";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={basename}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
